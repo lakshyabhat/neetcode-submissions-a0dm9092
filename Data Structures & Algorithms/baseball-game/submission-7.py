7f94cc50a80ebf650ebabe3e739e4f908a65600a
Lakshya Bhat <lakshyabhat@gmail.com>
2026-04-18 04:36:17 -0400

    Add: baseball-game - submission-7

---
+++ Data Structures & Algorithms/baseball-game/submission-7.py
@@ -0,0 +1,22 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        scoreStack = []
+        for o in operations:            
+            if o == '+':
+                if scoreStack:
+                    prev = scoreStack[-1]
+                    curr = scoreStack[-2]
+                    scoreStack.append(prev + curr)
+            elif o == 'C':
+                if scoreStack:
+                    scoreStack.pop()
+            elif o == 'D':
+                if scoreStack:
+                    scoreStack.append(scoreStack[-1]*2)
+            else:
+                val = int(o)
+                scoreStack.append(val)
+        print(scoreStack)
+        if scoreStack:
+            return sum(scoreStack)
+        return 0
